b58cd65af22ddc38bba35298b13efe6a9feec87a
Jeff Mitchell <crusty.rustacean@gmail.com>
2025-11-14 06:26:07 -0800

    improvement: bump to 0.3.0-alpha.3 and finish integration test for HTML serving

--- tests/api/get_index.rs
+++ tests/api/get_index.rs
@@ -2,21 +2,26 @@
 
 // dependencies
 use crate::helpers::send_request;
-use rama::http::{BodyExtractExt, StatusCode};
+use rama::http::{
+    BodyExtractExt, StatusCode,
+    headers::{ContentType, HeaderMapExt, dep::mime},
+};
 
 #[tokio::test]
 async fn get_index_returns_200_ok_and_html() {
     // Act
     let response = send_request("/").await;
-    let (parts, body) = response.into_parts();
 
     // Assert
-    assert_eq!(StatusCode::OK, parts.status);
+    assert_eq!(StatusCode::OK, response.status());
     assert_eq!(
-        parts.headers.get("content-type").unwrap(),
-        "text/html; charset=utf-8"
+        response.headers().typed_get::<ContentType>().unwrap(),
+        ContentType::from(mime::TEXT_HTML_UTF_8)
     );
 
-    let response_body = body.try_into_string().await.expect("Unable to get a string from the message body");
+    let response_body = response
+        .try_into_string()
+        .await
+        .expect("Unable to get a string from the message body");
     assert!(response_body.contains("<h1>Hello, World!"));
 }
