3171dca97b361975c25831db1e0ee3ed1b087b13
Jeff Mitchell <crusty.rustacean@gmail.com>
2025-11-14 20:36:30 -0800

    improvement: add graceful shutdown

--- src/startup.rs
+++ src/startup.rs
@@ -3,24 +3,23 @@
 // dependencies
 use crate::config::Config;
 use crate::routes::{get_index, health_check};
-use rama::{http::server::HttpServer, http::service::web::Router, rt::Executor};
+use rama::{
+    error::BoxError,
+    graceful::Shutdown, http::server::HttpServer, http::service::web::Router, rt::Executor,
+    tcp::server::TcpListener,
+};
+use std::time::Duration;
 
 pub struct Application {
-    executor: Executor,
     pub router: Router<()>,
     pub config: Config,
 }
 
 impl Application {
     pub fn build(config: Config) -> Self {
-        let executor = Executor::default();
         let router = Self::app_web_service();
 
-        Self {
-            executor,
-            router,
-            config,
-        }
+        Self { router, config }
     }
 
     pub fn app_web_service() -> Router<()> {
@@ -29,9 +28,19 @@ impl Application {
             .get("/", get_index)
     }
 
-    pub async fn run(self) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-        HttpServer::auto(self.executor)
-            .listen(&self.config.address, self.router)
+    pub async fn run(self) -> Result<(), BoxError> {
+        let graceful = Shutdown::default();
+
+        let tcp_listener = TcpListener::bind(self.config.address).await?;
+
+        graceful.spawn_task_fn(async |guard| {
+            let exec = Executor::graceful(guard.clone());
+            let http_service = HttpServer::auto(exec).service(self.router);
+            tcp_listener.serve_graceful(guard, http_service).await;
+        });
+
+        graceful
+            .shutdown_with_limit(Duration::from_secs(30))
             .await?;
 
         Ok(())
